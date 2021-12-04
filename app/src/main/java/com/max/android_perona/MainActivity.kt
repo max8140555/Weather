dea05b2a03f19b2cfda321ccddea12295417accb
max8140555 <61774466+max8140555@users.noreply.github.com>
2021-12-04 22:28:43 +0800

    init

---
+++ app/src/main/java/com/max/android_perona/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.max.android_perona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
